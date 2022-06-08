cc64fb999d78ead1cedff14add11a6f9d5748272
Goosa2025 <gus21002@byui.edu>
2022-06-08 15:12:48 -0600

    troubleshooting Heroku

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
-
 app.use('/', require('./routes'))
-
-// app.get('/', (req, res) => {
-//     res.send("You've been servered, Ames Gusa!")
-// })
 
 app.listen(port, () => {
     console.log(`Example app listening port ${port}`)
