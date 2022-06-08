3d04c743093841076835b9ac6b6fd32ff16bb32d
Goosa2025 <gus21002@byui.edu>
2022-06-08 14:40:14 -0600

    Test setup, procfile added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-        res.send("You've been servered, Ames Gusa!");
+        res.send("You've been servered, Ames Gusa!!!!");
     });
 
 module.exports= routes;
